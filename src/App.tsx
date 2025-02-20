66169c126486191f9e152781b78aebe49804112a
Michael Louis <michaellouis157@gmail.com>
2025-02-20 09:11:19 -0500

    Added vercel anayltics

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import VideoGallery from './pages/VideoGallery';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/videos" element={<VideoGallery />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/videos" element={<VideoGallery />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   );
 }
 
